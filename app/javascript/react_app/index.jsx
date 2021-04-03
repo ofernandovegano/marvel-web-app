c6757a8938fa2fdd1f65c481297ba4c4db669acd
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-04-03 14:46:30 -0300

    React sucessfully connected with Ruby

---
+++ app/javascript/react_app/index.jsx
@@ -0,0 +1,40 @@
+// external modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createHistory as history } from 'history';
+import { reducer as formReducer } from 'redux-form';
+
+// internal modules
+import App from './components/app';
+
+// Initial State
+const initialState = {
+  heroes: []
+};
+
+// State and reducers
+const reducers = combineReducers({
+  heroes: (state = null, action) => state
+});
+
+//Middlewares
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers, initialState, middlewares)}>
+    <Router history={history}>
+      <div className="view-container">
+        <Switch>
+          <Route path="/" exact component={App} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
