95e5a81d7357d1ace592eae439234460cea6043b
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-04-06 20:11:40 -0300

    comics index page sone - api working with comics image

--- app/javascript/react_app/actions/index.js
+++ app/javascript/react_app/actions/index.js
@@ -7,11 +7,8 @@ export const FETCH_COMICS = 'FETCH_COMICS';
 console.log(process.env)
 const BASE_URL = 'https://gateway.marvel.com/v1/public/';
 const { REACT_APP_MARVEL_PRIVATE_KEY, REACT_APP_MARVEL_PUBLIC_KEY} = process.env
-
 let timeStamp = Math.floor(Date.now() / 1000)
 let md5Key = md5(`${timeStamp}${REACT_APP_MARVEL_PRIVATE_KEY}${REACT_APP_MARVEL_PUBLIC_KEY}`)
-
-
 
 export function fetchComics() {
   const url = `${BASE_URL}comics?ts=${timeStamp}&apikey=${REACT_APP_MARVEL_PUBLIC_KEY}&hash=${md5Key}`;
--- app/javascript/react_app/containers/comics_index.jsx
+++ app/javascript/react_app/containers/comics_index.jsx
@@ -17,7 +17,28 @@ class ComicsIndex extends Component {
         <Link to="/">
           <p>Back to Marvel Index</p>
         </Link>
-
+        <div className="comics">
+            {this.props.comics.map((comic) => {
+              return (
+              < div key = { comic.id } className = "comic" >
+              
+                <Link to={`/comics/${comic.id}`} key={comic.id}>
+                  < div>
+                    {console.log(comic)}
+                    <div className="comic-img-div">
+                        <img src={`${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`} className="comic-img" />
+                      
+                    </div>
+                    <div className="comic-title">
+                        {comic.title.length > 30 ? `${comic.title.slice(0, 30)}...` : comic.title }
+                    </div>
+                  </div>
+                  </Link>
+                  
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   };
