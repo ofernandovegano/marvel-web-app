964b3781616e0b219f2289f3c8ce622e99d18663
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-04-07 23:55:04 -0300

    setup the favorite_comics and favorite_characters models

--- app/models/favorite_character.rb
+++ app/models/favorite_character.rb
@@ -1,3 +1,3 @@
 class FavoriteCharacter < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
 end
--- app/models/favorite_comic.rb
+++ app/models/favorite_comic.rb
@@ -1,3 +1,3 @@
 class FavoriteComic < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
 end
